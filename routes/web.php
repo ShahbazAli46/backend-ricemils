f2fd9fafdeaab093822cabc4e1b0b80c6611e411
hamza2236 <hamza502m@gmail.com>
2024-08-21 15:47:20 +0500

    add clear cache route

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,26 @@ Route::get('migrate', function(){
         echo "Failed to execute migrations: " . $e->getMessage();
     }
 });
+
+Route::get('clear', function(){
+    try {
+        Artisan::call('config:clear');
+        $output = Artisan::output();
+        echo "Configuration cache cleared:\n$output";
+        
+        Artisan::call('cache:clear');
+        $output = Artisan::output();
+        echo "Application cache cleared:\n$output";
+        
+        Artisan::call('route:clear');
+        $output = Artisan::output();
+        echo "Route cache cleared:\n$output";
+        
+        Artisan::call('view:clear');
+        $output = Artisan::output();
+        echo "View cache cleared:\n$output";
+    } catch (\Exception $e) {
+        // Handle any exceptions
+        echo "Failed to execute migrations: " . $e->getMessage();
+    }
+});
